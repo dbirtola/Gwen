cb4016bcc4029fc73c34fc9aac3d8c8b5508dea9
redhatdragon <paulabrams99@yahoo.com>
2022-01-21 04:51:45 -0500

    Created PooledActor
    Pooled actor is currently unusable

---
+++ Source/ProjectGwen/Pool/PooledActor.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ShapeComponent.h"
+#include "UObject/Object.h"
+#include "PooledActor.generated.h"
+
+UCLASS()
+class PROJECTGWEN_API UPooledActor : public UObject {
+	GENERATED_BODY()
+public:
+	UPooledActor() {
+		throw;  //TODO: replace with print message.
+	}
+};
