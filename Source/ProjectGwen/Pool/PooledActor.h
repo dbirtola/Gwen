c2321bc273333ad2818ffdd3d6c1eebb04d8d9bc
redhatdragon <paulabrams99@yahoo.com>
2022-01-21 07:05:05 -0500

    Fixed PooledActor, Added FlatBuffer.h
    *Converted PooledActor to inherit from AActor instead of UObject
    *Added FlatFlaggedBuffer and it's BitBool dependency

--- Source/ProjectGwen/Pool/PooledActor.h
+++ Source/ProjectGwen/Pool/PooledActor.h
@@ -3,16 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/ShapeComponent.h"
-#include "UObject/Object.h"
+#include "GameFramework/Actor.h"
 #include "../deps/IO/IO.h"
 #include "PooledActor.generated.h"
 
 UCLASS()
-class PROJECTGWEN_API UPooledActor : public UObject {
+class PROJECTGWEN_API APooledActor : public AActor {
 	GENERATED_BODY()
 public:
-	UPooledActor() {
+	// Sets default values for this actor's properties
+	APooledActor() {
 		printErrorToScreen("UPooledActor not ready for use yet!");
+	}
+
+protected:
+	virtual void BeginPlay() override {
+		Super::BeginPlay();  //TODO: Is this needed?  Can this be set to = nullptr?
+	}
+
+public:
+	virtual void Tick(float DeltaTime) override {
+		Super::Tick(DeltaTime);  //TODO: Is this needed?
 	}
 };
