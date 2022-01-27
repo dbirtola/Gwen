2ee2e61484aeddf80f2ab8c63be15c011d4b8007
redhatdragon <paulabrams99@yahoo.com>
2022-01-27 05:43:19 -0500

    Cleanup

--- Source/ProjectGwen/Plant/PlantFunctionLibrary.cpp
+++ Source/ProjectGwen/Plant/PlantFunctionLibrary.cpp
@@ -4,23 +4,23 @@
 #include "PlantFunctionLibrary.h"
 #include "../deps/FlatBuffer/FlatBuffer.h"
 #include "../GAME_LIMITS.h"
-#include "../deps/IO/IO.h"
+#include "PaulsUtilities/Public/IOFunctionLibrary.h"
 
 FlatBuffer<APlantActor*, GAME_LIMITS::MAX_STATIC_ACTOR_COUNT> plantPool;
 
 APlantActor* UPlantFunctionLibrary::PlantCreate(TSubclassOf<APlantActor> ClassType, AActor* SpawningActor, const FTransform& Transform) {
 	UWorld* world = SpawningActor->GetWorld();
 	if(ClassType == nullptr) {
-		printErrorToScreen("PlantFunctionLibrary::PlantCreate()'s ClassType was nullptr");
+		UIOFunctionLibrary::printErrorToScreen("PlantFunctionLibrary::PlantCreate()'s ClassType was nullptr");
 		return nullptr;  //TODO: Decide if we should just throw here instead.
 	}
 	APlantActor* plant = Cast<APlantActor>(UActorPoolFunctionLibrary::actorPoolGetActor(ClassType, world, Transform));
 	if (plant == nullptr) {
-		printErrorToScreen("PlantFunctionLibrary::PlantCreate() could not get another pooled APlantActor");
+		UIOFunctionLibrary::printErrorToScreen("PlantFunctionLibrary::PlantCreate() could not get another pooled APlantActor");
 		return nullptr;  //TODO: Decide if we should just throw here instead.
 	}
 	if (plantPool.count == GAME_LIMITS::MAX_STATIC_ACTOR_COUNT) {
-		printErrorToScreen("PlantFunctionLibrary::PlantCreate() has already reached it's max APlantActor count");
+		UIOFunctionLibrary::printErrorToScreen("PlantFunctionLibrary::PlantCreate() has already reached it's max APlantActor count");
 		return nullptr;  //TODO: Decide if we should just throw here instead.
 	}
 	plantPool.push(plant);
